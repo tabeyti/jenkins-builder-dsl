7550bb8bb5e62d88d718537f08628e625430dcda
tabeyti <tabeyti@gmail.com>
2020-10-07 09:02:08 -0700

    organizing task steps into it's own class
    pulled in implementation from old 'ham' step in tool-jenkins: https://github.com/tabeyti/tool-jenkins/commit/87535128ddaad0d60386e042b367532463d6dcfc#diff-8e68e887d6d4fe42e6e79b81d1afe74e
    
    preparing for creating 'run' method for task (within TaskDSL)

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -80,109 +80,362 @@ abstract class DslBase implements Serializable {
 
 } // class DslBase
 
+@DslClass
+class TaskSteps extends DslBase {
+    public String description
+    public List sh = []
+    public List bat = []
+    public List env = []
+    public List creds = []
+    public def archive
+    public Map build
+    public Closure closure
+    public Map p4s
+    public Map p4cl
+    public def git
+    public def svn
+    public def checkout
 
-/**
- * DSL 'task' object.
- * Holds properties specific to running a
- * body of work on an agent/node.
- *
- * This was created as a base class to be
- * used by (potentially) other custom DSL type
- * 'steps'.
- */
-class TaskDSL extends DslBase {
-  public String name
-  public String node
-  public String sh
-  public String bat
-  public List env = []
-  public List creds = []
-  public String archive
-  public Map build
-  public def git
-  public String svn
+    public TaskSteps(def script) {
+        super(script)
+    }
+
+    def TaskSteps(def script, String name) {
+        super(script, name)
+    }
+
+    @DslStep
+    def archive(String pattern) {
+        assertNull(this.archive, 'archive')
+        logger.debug "archive: $pattern"
+        this.archive = [artifacts: pattern]
+    }
+
+    @DslStep
+    def archive(Map map) {
+        assertNull(this.archive, 'archive')
+        logger.debug "archive: $map"
+        this.archive = map
+    }
+
+    @DslStep
+    def bat(String cmd) {
+        logger.debug "bat: $cmd"
+        this.bat.add(cmd)
+    }
+
+    @DslStep
+    def bat(List cmds) {
+        logger.debug "bat: $cmds"
+        this.bat = this.bat + cmds
+    }
+
+    @DslStep
+    def bat(Map map) {
+        logger.debug "bat: $map"
+        this.bat.add(map)
+    }
+
+    @DslStep
+    def build(Map build) {
+        assertNull(this.build, 'build')
+        logger.debug "build: $build"
+        this.build = build
+    }
+
+    @DslStep
+    def build(String build) {
+        assertNull(this.build, 'build')
+        logger.debug "build: $build"
+        this.build = [job: build, parameters: []]
+    }
+
+    @DslStep
+    def closure(Closure c) {
+        assertNull(this.closure, 'closure')
+        logger.debug "closure: closure"
+        this.closure = c
+        this.closure.delegate = this
+        this.closure.owner = this
+    }
+
+    @DslStep
+    def creds(Object...creds) {
+        logger.debug "creds: $creds"
+        this.creds = creds
+    }
+
+    @DslStep
+    def checkout(Map checkout) {
+        assertNull(this.checkout, 'checkout')
+        logger.debug "checkout: ${checkout}"
+        this.checkout = checkout
+    }
+
+    @DslStep
+    def description(String description) {
+        assertNull(this.description, 'description')
+        logger.debug "description: ${description}"
+        this.description = description
+    }
+
+    @DslStep
+    def desc(String description) {
+        assertNull(this.description, 'description')
+        logger.debug "description: ${description}"
+        this.description = description
+    }
+
+    @DslStep
+    def env(Map env) {
+        logger.debug "env: $env"
+        def envList = script.utils.mapToEnv(env)
+        this.env.addAll(envList)
+    }
+
+    @DslStep
+    def env(String...env) {
+        logger.debug "env: $env"
+        this.env.addAll(env)
+    }
+
+    @DslStep
+    def git(Object git) {
+        assertNull(this.git, 'git')
+        logger.debug "git: ${git}"
+        this.git = git
+    }
+
+    @DslStep
+    def p4s(Map map) {
+        assertNull(this.p4s, 'p4s')
+        logger.debug "p4s: $map"
+        this.p4s = map
+    }
+
+    @DslStep
+    def p4cl(Map map) {
+        assertNull(this.p4cl, 'p4cl')
+        logger.debug "p4cl: $map"
+        this.p4cl = map
+    }
+
+    @DslStep
+    def sh(String cmd) {
+        logger.debug "sh: $cmd"
+        this.sh.add([script: cmd])
+    }
+
+    @DslStep
+    def sh(List cmds) {
+        logger.debug "sh: $cmds"
+        def map = [:]
+        cmds.each { this.sh.add([script: it]) }
+    }
+
+    @DslStep
+    def sh(Map map) {
+        logger.debug "sh: $map"
+        this.sh.add(map)
+    }
+
+    /**
+    * Either prepends or assigns (if not null) the given steps
+    * to this task's steps.
+    */
+    def combineTaskSteps(TaskSteps otherTask) {
+        if (!otherTask) { return }
+
+        this.sh = otherTask.sh + this.sh
+        this.bat = otherTask.bat + this.bat
+        this.env = otherTask.env + this.env
+        this.creds = otherTask.creds + this.creds
+        this.archive = this.archive ?: otherTask.archive
+        this.build =  this.build ?: otherTask.build
+        this.closure = this.closure ?: otherTask.closure?.clone()
+        this.p4s = this.p4s ?: otherTask.p4s
+        this.p4cl = this.p4cl ?: otherTask.p4cl
+        this.git = this.git ?: otherTask.git
+        this.svn = this.svn ?: otherTask.svn
+        this.checkout = this.checkout ?: otherTask.checkout
+    }
+
+    /**
+    * Returns a string of all the step values in this instance.
+    */
+    String getStr() {
+        def str = ''
+        def tabs = ''
+        // (0..numTabs).each { tabs += '\t' }
+        str += "${tabs}description: ${this.description}\n"
+        str += "${tabs}sh: ${this.sh}\n"
+        str += "${tabs}bat: ${this.bat}\n"
+        str += "${tabs}env: ${this.env}\n"
+        str += "${tabs}creds: ${this.creds}\n"
+        str += "${tabs}archive: ${this.archive}\n"
+        str += "${tabs}build: ${this.build}\n"
+        // str += "${tabs}closure: ${this.closure}\n"
+        str += "${tabs}p4s: ${this.p4s}\n"
+        str += "${tabs}p4cl: ${this.p4cl}\n"
+        str += "${tabs}git: ${this.git}\n"
+        str += "${tabs}svn: ${this.svn}\n"
+        str += "${tabs}checkout: ${this.checkout}\n"
+        return str
+    }
+
+    /**
+    * Clones the current steps into a new object.
+    */
+    TaskSteps clone() {
+        TaskSteps clonedTask = new TaskSteps(this.script)
+        clonedTask.sh = this.sh.clone()
+        clonedTask.bat = this.bat.clone()
+        clonedTask.env = this.env.clone()
+        clonedTask.creds = this.creds.clone()
+        clonedTask.archive = this.archive?.clone()
+        clonedTask.build = this.build?.clone()
+        clonedTask.closure = this.closure?.clone()
+        clonedTask.p4s = this.p4s?.clone()
+        clonedTask.p4cl = this.p4cl?.clone()
+        clonedTask.git = this.git?.clone()
+        clonedTask.svn = this.svn?.clone()
+        clonedTask.checkout = this.checkout?.clone()
+        return clonedTask
+    }
+
+    def evaluate() { }
+}
+
+class TaskDSL extends TaskSteps {
+
+    public String node = 'master'
+    public Map axes
+    // Task lists
+    public List posts = []
+    public List tasks = []
+
+    public final boolean isBuilderTask = false
 
     def TaskDSL(def script) {
         super(script)
     }
 
     def TaskDSL(def script, String name) {
-        super(script)
-        this.name = name
+        super(script, name)
     }
 
+    def TaskDSL(def script, String name, boolean isBuilderTask) {
+        super(script, name)
+        this.isBuilderTask = isBuilderTask
+    }
+
+    @DslStep
+    def axes(Map config) {
+        assertNull(this.axes, 'axes')
+        logger.debug "axes: $config"
+        this.axes = config
+    }
+
+    @DslStep
     def name(String name) {
         assertNull(this.name, 'name')
-        debug "name: $name"
         this.name = name
     }
 
+    @DslStep
     def node(String node) {
-        assertNull(this.node, 'node')
-        debug "node: $node"
+        if ('master' != this.node) {
+            assertNull(this.node, 'node')
+        }
+        logger.debug "node: $node"
         this.node = node
     }
 
-    def git(Object git) {
-        assertNull(this.git, 'git')
-        debug "git: ${git}"
-        this.git = git
+    @DslStep
+    def post(Closure body) {
+        logger.debug 'post'
+        def t = new TaskDSL(this.script)
+        body.delegate = t
+        body()
+        this.posts.add(t)
     }
 
-    def sh(String cmd) {
-        assertNull(this.sh, 'sh')
-        debug "${this.name} - sh: ${cmd}"
-        this.sh = cmd
+    @DslStep
+    def post(String name, Closure body) {
+        logger.debug "post: ${name}"
+        def t = new TaskDSL(this.script, name)
+        body.delegate = t
+        body()
+        this.posts.add(t)
     }
 
-    def sh(List cmds) {
-        assertNull(this.sh, 'sh')
-        debug "${this.name} - sh: ${cmds}"
-        this.sh = cmds.join('\n')
+    @DslStep
+    def task(Closure body) {
+        logger.debug 'task'
+        def t = new TaskDSL(this.script)
+        body.delegate = t
+        body()
+        this.tasks.add(t)
     }
 
-    def bat(String cmd) {
-        assertNull(this.bat, 'bat')
-        debug "${this.name} - bat: ${cmd}"
-        this.bat = cmd
+    @DslStep
+    def task(String name, Closure body) {
+        logger.debug "task: ${name}"
+        def t = new TaskDSL(this.script, name)
+        body.delegate = t
+        body()
+        this.tasks.add(t)
     }
 
-    def bat(List cmds) {
-        assertNull(this.bat, 'bat')
-        debug "${this.name} - bat: ${cmds}"
-        this.bat = cmds.join('\n')
+    String getStr() {
+        def str = '\n'
+        def tabs = ''
+        // (0..numTabs).each { tabs += '\t' }
+        str += "${tabs}name: ${this.name}\n"
+        str += "${tabs}axes: ${this.axes}\n"
+        str += super.getStr()
+
+        // Task lists
+        str += "${tabs}posts: ${this.posts.size()}\n"
+        str += "${tabs}tasks: ${this.tasks.size()}\n"
+        str += "${tabs}share: ${null != this.share}\n"
+        str += "${tabs}notify: ${!this.notify.isEmpty()}\n"
+        return str
     }
 
-    def creds(Object...creds) {
-        debug "creds: $creds"
-        this.creds = creds
-    }
+    TaskDSL clone() {
+        TaskDSL clonedTask = new TaskDSL(this.script, this.name)
+        clonedTask.combineTaskSteps(super.clone())
 
-    def env(String...env) {
-        debug "env: $env"
-        this.env.addAll(env)
-    }
+        clonedTask.name = this.name
+        clonedTask.axes = this.axes?.clone()
+        clonedTask.node = this.node
 
-    def archive(String pattern) {
-        assertNull(this.archive, 'archive')
-        debug "archive: $pattern"
-        this.archive = pattern
+        // Recursively clone child tasks. Science!
+        this.tasks.each { clonedTask.tasks.add(it.clone()) }
+        this.posts.each { clonedTask.posts.add(it.clone()) }
+        this.notify.each { clonedTask.notify.add(it.clone()) }
+        clonedTask.share = this.share?.clone()
+
+        return clonedTask
     }
 
     def evaluate() {
-        if (null == this.node) {
-            throw new Exception('Must provide a node for a task.')
+        if (!this.name) {
+            throw new Exception('Must provide a name for a task (excluding top level "builder" call).')
         }
-        if (null == this.name) {
-            throw new Exception('Must provide a name for a task.')
-        }
+
+        (this.tasks + this.posts).each { it.evaluate() }
     }
 } // class TaskDSL
 
+// builder(String name, Closure body) {
 def call(String name, Closure body) {
     // TODO
 }
 
+
+// def builder(String name) {
 def call(Closure body) {
     // TODO
 }
