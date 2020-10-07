0447b2b5af2ad1c55e3405ea41d771464073871e
tabeyti <tabeyti@gmail.com>
2020-10-07 08:43:28 -0700

    updated DslBase to use logger

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -1,55 +1,80 @@
+
+#!/usr/bin/env groovy
+
 /**
- * Base for the DSL classes.
- */
+* Builder DSL classes and step(s)
+* TODO:
+*   - validate svn usage
+*/
+
+// For debugging, adjust the logging level here
+// to affect global DSL class logging level
+// [NONE, FATAL, ERROR, WARN, INFO, DEBUG, TRACE, ALL]
+@Field LOG_LEVEL = 'INFO'
+
+/**
+* Abstract class for all DSL blocks.
+*/
 abstract class DslBase implements Serializable {
-  protected def script
-  public static Boolean DEBUG_ENABLED = false
+    protected def script
+    protected String name
+    public Logger logger
 
-  /**
-   * Base constructor.
-   * @param  script [description]
-   */
-  public DslBase(def script) {
-    this.script = script
-  }
-
-  /**
-   * Wrapper around script's println. Used for debug output.
-   * @param  message [description]
-   */
-  protected debug(String message) {
-    if (DslBase.DEBUG_ENABLED) {
-      this.script.println message
+    public DslBase(def script) {
+        this.script = script
+        this.logger = new Logger(script, script.LOG_LEVEL)
     }
-  }
 
-  /**
-   * Check to assert that the property is null. Generally
-   * called at the beginning of a DSL property method if
-   * the implementer wants to ensure an exception is thrown
-   * if the property was called twice.
-   * @param  val        The value
-   * @param  propString The property name
-   * @param  message    Custom message to display, otherwise a default exception
-   * @return            Throws an Exception if value is not null
-   */
-  protected def assertNull(def val, String propString, String message = null) {
-    if (null != val) {
-      if (null == message) {
-        throw new Exception("cannot define ${propString} twice.")
-      }
-      else {
-        throw new Exception(message)
-      }
+    public DslBase(def script, String name) {
+        this.script = script
+        this.name = name
+        this.logger = new Logger(script, script.LOG_LEVEL)
+        this.logger.module = name
     }
-  }
 
-  /**
-   * Evaluates the DSL property values for correctness when
-   * called, throwing an exception if invalid.
-   */
-  def abstract evaluate()
+    public String getName() {
+        return this.name
+    }
+
+    public String setName(String n) {
+        this.logger.module = n
+        this.name = n
+    }
+
+    /**
+    * Check to assert that the property is null (assinged once). Generally
+    * called at the beginning of a DSL property method if
+    * the implementer wants to ensure an exception is thrown
+    * if the property was called twice.
+    * @param  val        The value
+    * @param  propString The property name
+    * @param  message    Custom message to display, otherwise a default exception
+    * @return            Throws an Exception if value is not null
+    */
+    protected def assertNull(def val, String propString, String message = null) {
+        if (null != val) {
+            if (!message) {
+                throw new Exception("cannot define ${propString} twice.")
+            }
+            else {
+                throw new Exception(message)
+            }
+        }
+    }
+
+    /**
+    * Evaluates the DSL property values for correctness when
+    * called, throwing an exception if invalid.
+    */
+    def abstract evaluate()
+
+    /**
+    * Clones the DSL. Yep.
+    */
+    def abstract clone()
+
 } // class DslBase
+
 
 /**
  * DSL 'task' object.
