8757f214efeca15b06ec58597afcd6a814ea003e
tabeyti <tabeyti@gmail.com>
2020-10-08 22:09:11 -0700

    fix of rejected access exception due to enum

--- src/org/tools/Logger.groovy
+++ src/org/tools/Logger.groovy
@@ -1,67 +1,63 @@
 // #!/usr/bin/groovy
 package org.tools
 
+
 class Logger implements Serializable {
-    enum Level {
-        NONE(0),
-        FATAL(1),
-        ERROR(2),
-        WARN(3),
-        INFO(4),
-        DEBUG(5),
-        TRACE(6),
-        ALL(7)
-
-        private int value
-
-        Level(int val) {
-            this.value = val
-        }
-    }
 
     public String module
     private def script
-    private Level level = Level.ERROR
+    private final Map levels = [
+        NONE: 0,
+        FATAL: 1,
+        ERROR: 2,
+        WARN: 3,
+        INFO: 4,
+        DEBUG: 5,
+        TRACE: 6,
+        ALL: 7
+    ]
+    private String level = 'ERROR'
 
-    def Logger(def script, String level = Level.ERROR.toString()) {
+
+    def Logger(def script, String level = 'ERROR') {
         this.script = script
-        this.level = level as Level
+        this.level = level
     }
 
     def trace(String message) {
-        this.log(Level.TRACE, message)
+        this.log('TRACE', message)
     }
 
     def debug(String message) {
-        this.log(Level.DEBUG, message)
+        this.log('DEBUG', message)
     }
 
     def info(String message) {
-        this.log(Level.INFO, message)
+        this.log('INFO', message)
     }
 
     def warn(String message) {
-        this.log(Level.WARN, message)
+        this.log('WARN', message)
     }
 
     def error(String message) {
-        this.log(Level.ERROR, message)
+        this.log('ERROR', message)
     }
 
     def fatal(String message) {
-        this.log(Level.FATAL, message)
+        this.log('FATAL', message)
     }
 
     String getLevel() {
-        return this.level.toString()
+        return this.level
     }
 
     String setLevel(String level) {
-        this.level = level as Level
+        this.level = level
     }
 
-    private def log(Level level, String message) {
-        if (level > this.level) { return }
+    private def log(String level, String message) {
+        if (this.levels[level] > this.levels[this.level]) { return }
 
         def line = "[$level]: "
         line += (this.module) ? "${this.module} - " : ''
